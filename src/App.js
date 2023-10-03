88b93981c962cd850a6cf33aca5f8e0eb09d3d79
Reese <arter.charise@gmail.com>
2023-10-02 21:18:26 -0700

    practicing class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-   <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: 'Reese',
+		};
+	}
+	render() {
+		return (
+			<div className='App'>
+				<h1>Hello, {this.state.name}</h1>
+				<button onClick={() => this.setState({ name: 'Fox' })}>
+					Click to change Name
+				</button>
+			</div>
+		);
+	}
 }
 
 export default App;
