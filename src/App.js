30c4c890eefa5f680a3180c9a7d4338d11c9413b
Reese <arter.charise@gmail.com>
2023-10-04 20:19:35 -0700

    adding search functionality

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ class App extends Component {
 		const { users } = this.state;
 		return (
 			<div className='App'>
+				<input
+					className='search-box'
+					type='text'
+					placeholder='Search for GitHub User'
+					onChange={(e) => {
+						// filter thru users and get what you want
+					}}
+				/>
 				{users.map((user) => (
 					<h1 key={user.id}>{user.login}</h1>
 				))}
